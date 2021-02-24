1648e44e1706770a9c8bd36c77fc7a86d439a525
Ranger Chenore <r2chenore@gmail.com>
2021-02-23 19:22:51 -0500

    Add physics and collisions

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     float walkSpeed = 10f;
+    bool shouldMove = false;
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start() {
-
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update() {
+        shouldMove = false;
         if (Input.GetKey(KeyCode.W)) {
+            shouldMove = true;
+        }
+    }
 
-            Vector3 newPos = transform.position + (transform.forward * walkSpeed * Time.deltaTime);
-            transform.position = newPos;
+    private void FixedUpdate() {
+        if (shouldMove) {
+            Vector3 dir = transform.forward * walkSpeed * Time.fixedDeltaTime;
+            rb.AddForce(dir, ForceMode.VelocityChange);
         }
     }
 }
